a34f2e33ab638cad5a1847a725ef2085907d1e0b
Manuela <manuela.zapata.atencia@gmail.com>
2023-04-02 01:07:26 -0500

    Creation of the ConcertDB entity with its attributes

---
+++ Parcial2_ZAPATA_ATENCIA_MANUELA/DAL/Entities/ConcertDB.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Parcial2_ZAPATA_ATENCIA_MANUELA.DAL.Entities
+{
+    public class ConcertDB
+    {
+        [Key]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public Guid id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime? UseDate { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool IsUsed  { get; set; } = false;
+
+        [Display(Name = "Nombre de portería")]
+        [MaxLength(10)]
+        [Required (ErrorMessage = "El campo {0} es obligatorio")]
+        public string? EntranceGate { get; set; }
+
+    }
+}
